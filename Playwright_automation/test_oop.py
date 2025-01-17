1040f96b3ada391305a9ca87f8fe1d773361bf92
Tamir Dayan <tamir.dayan@ridewithvia.com>
2025-01-18 00:47:15 +0200

    Restored folder structure

--- Playwright_automation/test_oop.py
+++
@@ -1,5 +0,0 @@
-from Playwright_automation import oop
-
-def test_oop():
-    iphone = oop.MobileDevice("iphone", "iOS", 14, True, 6000)
-    iphone.print_parameters()
--- automation_final_project/questions/pokemon_damage_calculator/pokemon.py
+++
@@ -1,7 +0,0 @@
-from automation_final_project.questions.pokemon_damage_calculator import pokemon_action
-
-
-class Pokemon:
-    def __init__(self, pokemon_name):
-        self.name = pokemon_name
-        self.action = pokemon_action.PokemonAction()
